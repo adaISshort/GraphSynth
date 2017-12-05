76dccf05a97b3704c266ebaf9d2e5782c9abf42f
Alex Turner <turneral1@grinnell.edu>
2017-12-05 12:50:29 -0800

    Implemented bandit algorithms in C# (using PyPlan as a blueprint).

---
+++ GraphSynthSourceFiles/GraphSynth.BaseClasses/SearchProcess/Bandits/AbstractBandit.cs
@@ -0,0 +1,62 @@
+﻿using System.Linq;
+
+
+namespace GraphSynth.Search.Bandits {
+    public class AbstractBandit {
+        public int numArms;
+        public int totalPulls;
+        public int[] numPulls; // how many times we've pulled each arm
+        public double[] averageReward;
+
+        /// <summary>
+        /// Initialize a new AbstractBandit.
+        /// </summary>
+        /// <param name="numArms_">How many actions are available at the corresponding state.</param>
+        public AbstractBandit(int numArms_) {
+            numArms = numArms_;
+            averageReward = new double [numArms]; // rewards initialized to 0
+            numPulls = new int [numArms];
+        }
+
+        /// <summary>
+        /// Update the given arm's pull count, its average reward, and the total pull count.
+        /// </summary>
+        /// <param name="arm">The index of the arm to update.</param>
+        /// <param name="reward">The observed reward.</param>
+        public void Update(int arm, int reward) {
+            numPulls[arm]++;
+            averageReward[arm] = (averageReward[arm] * (numPulls[arm] - 1) + reward) / numPulls[arm];
+            totalPulls++;
+        }
+
+        /// <summary>
+        /// Get the arm with the best average reward.
+        /// </summary>
+        public int GetBestArm() {
+            var max = double.NegativeInfinity;
+            var maxArm = 0;
+            for (var i = 0; i < numArms; i++) {
+                if (averageReward[i] > max) {
+                    max = averageReward[i];
+                    maxArm = i;
+                }
+            }
+
+            return maxArm;
+        }
+
+        /// <summary>
+        /// Return the best average reward.
+        /// </summary>
+        public double GetBestReward() {
+            return averageReward.Max();
+        }
+
+        /// <summary>
+        /// Selects and returns a pull arm pursuant to the details of the bandit algorithm.
+        /// </summary>
+        public int SelectPullArm() {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ GraphSynthSourceFiles/GraphSynth.BaseClasses/SearchProcess/Bandits/EGreedyBandit.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace GraphSynth.Search.Bandits {
+    public class EGreedyBandit : AbstractBandit {
+        private double _epsilon;
+        private readonly Random _rand = new Random();
+
+        public EGreedyBandit(int numArms_, double epsilon_) : base(numArms_) {
+            _epsilon = epsilon_;
+        }
+
+        /// <summary>
+        /// Pulls the most rewarding arm with (1 - epsilon) probability; otherwise, a non-optimal arm is pulled at random.
+        /// </summary>
+        public int SelectPullArm() {
+            if (numArms <= 1) {
+                return 0; // return the only arm we can
+            }
+
+            if (totalPulls < numArms) {
+                return totalPulls; // haven't tried each arm at least once yet
+            }
+
+            var bestArm = GetBestArm();
+            if (_rand.NextDouble() < _epsilon) {
+                var nonBest = _rand.Next(numArms);
+                while (nonBest == bestArm) {
+                    nonBest = _rand.Next(numArms);
+                }
+                return nonBest; // pull a random non-best arm
+            } 
+            
+            return bestArm;
+        }
+    }
+}
